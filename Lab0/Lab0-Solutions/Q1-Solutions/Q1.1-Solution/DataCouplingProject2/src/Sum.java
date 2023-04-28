3662163b85b3b37a5a6c53a5432ba897c597605d
RanShuai 2633609R <123078993+1RanShy@users.noreply.github.com>
2023-04-28 16:36:12 +0100

    Delete Lab0 directory

--- Lab0/Lab0-Solutions/Q1-Solutions/Q1.1-Solution/DataCouplingProject2/src/Sum.java
+++
@@ -1,8 +0,0 @@
-public class Sum {
-	public void sum(NumbersAndBoolean numsAndBool) {
-		int result = numsAndBool.getFirstNum() + numsAndBool.getSecondNum();
-		System.out.println("sum: " + result);
-	}
-}
-
-
